032e4f3a2897099f646c4a17421645e499bc967b
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-05 13:59:30 +0530

    Add css

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = () => {
+	return (
+		<div>
+			<div>March 28th 2022</div>
+			<div>
+				<h2>Car Insurance</h2>
+			</div>
+			<div>Rs 2000</div>
+		</div>
+	);
+};
+
+export default ExpenseItem;
