38a74be27d64707cb6cf0d8493e13b38810c57ee
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-06 11:45:54 +0530

    splite Expense Item component in Expense date component and Add Style For expense date component

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
 			<div className="expense-item__description ">
+				<ExpenseDate date={props.date} />
 				<h2>{props.title}</h2>
 			</div>
 			<div className="expense-item__price">Rs {props.price}</div>
