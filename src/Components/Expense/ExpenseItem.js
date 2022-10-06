33c6915ff349184e3bed5778a6fa624332738eaf
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-06 15:56:32 +0530

    Add State

--- src/Components/Expense/ExpenseItem.js
+++ src/Components/Expense/ExpenseItem.js
@@ -1,13 +1,20 @@
 import "./ExpenseItem.css";
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
+	// React Hooks return array we get the value using array destracturing
+	const [title, setTitle] = useState(props.title);
+	const clickHandler = () => {
+		setTitle("Ashish Gaikwad");
+	};
 	return (
 		<div className="expense-item">
 			<div className="expense-item__description ">
 				<ExpenseDate date={props.date} />
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 			</div>
 			<div className="expense-item__price">Rs {props.price}</div>
+			<button onClick={clickHandler}>Chnage Title </button>
 		</div>
 	);
 };
