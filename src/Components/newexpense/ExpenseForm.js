c4e40d17f7c58c3d357d8d393753d1f893cb86bc
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-07 12:19:47 +0530

    listen user inputs values

--- src/Components/newexpense/ExpenseForm.js
+++ src/Components/newexpense/ExpenseForm.js
@@ -1,19 +1,40 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 const ExpenseForm = () => {
+	const [title, setTitle] = useState("");
+	const [date, setDate] = useState("");
+	const [price, setPrice] = useState("");
+	// Chnage title Handler
+	const titleChnageHandler = (e) => {
+		setTitle(e.target.value);
+	};
+
+	const amountChnageHandler = (e) => {
+		setPrice(e.target.value);
+	};
+
+	const dateChnageHandler = (e) => {
+		setDate(e.target.value);
+	};
 	return (
 		<form>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" />
+					<input type="text" onChange={titleChnageHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" />
+					<input type="number" onChange={amountChnageHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31" />
+					<input
+						type="date"
+						min="2019-01-01"
+						max="2022-12-31"
+						onChange={dateChnageHandler}
+					/>
 				</div>
 			</div>
 			<div className="new-expense__actions ">
