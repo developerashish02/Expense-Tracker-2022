ff8f7b5eaac1b9a471e777fed71bb725fc7de364
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-10-08 20:11:54 +0530

    Add from cancel and show form button

--- src/Components/newexpense/ExpenseForm.js
+++ src/Components/newexpense/ExpenseForm.js
@@ -4,6 +4,7 @@ const ExpenseForm = (props) => {
 	const [title, setTitle] = useState("");
 	const [date, setDate] = useState("");
 	const [price, setPrice] = useState("");
+	const [form, setForm] = useState(false);
 
 	// Chnage title Handler
 	const titleChnageHandler = (e) => {
@@ -17,50 +18,72 @@ const ExpenseForm = (props) => {
 	const dateChnageHandler = (e) => {
 		setDate(e.target.value);
 	};
-
 	// submit date
 	const submitHandler = (e) => {
 		e.preventDefault();
-
 		const expense = {
 			title,
 			date: new Date(date),
 			price,
 		};
-
 		props.onSaveExpenseData(expense);
-
 		// Clear inputs
 		setTitle("");
 		setDate("");
 		setPrice("");
 	};
+	const showFormJSX = () => {
+		setForm(true);
+	};
+
+	const cancelFormHandler = () => {
+		setForm(false);
+	};
 	return (
-		<form onSubmit={submitHandler}>
-			<div className="new-expense__controls">
-				<div className="new-expense__control">
-					<label>Title</label>
-					<input type="text" onChange={titleChnageHandler} value={title} />
+		<>
+			{form && (
+				<form onSubmit={submitHandler}>
+					<div className="new-expense__controls">
+						<div className="new-expense__control">
+							<label>Title</label>
+							<input type="text" onChange={titleChnageHandler} value={title} />
+						</div>
+						<div className="new-expense__control">
+							<label>Amount</label>
+							<input
+								type="number"
+								onChange={amountChnageHandler}
+								value={price}
+							/>
+						</div>
+						<div className="new-expense__control">
+							<label>Date</label>
+							<input
+								type="date"
+								min="2019-01-01"
+								max="2022-12-31"
+								onChange={dateChnageHandler}
+								value={date}
+							/>
+						</div>
+					</div>
+					<div className="new-expense__actions ">
+						<button type="submit">Add Expense</button>
+						<button type="button" onClick={cancelFormHandler}>
+							Cancel
+						</button>
+					</div>
+				</form>
+			)}
+
+			{!form && (
+				<div>
+					<button type="submit" onClick={showFormJSX}>
+						Add New Expense
+					</button>
 				</div>
-				<div className="new-expense__control">
-					<label>Amount</label>
-					<input type="number" onChange={amountChnageHandler} value={price} />
-				</div>
-				<div className="new-expense__control">
-					<label>Date</label>
-					<input
-						type="date"
-						min="2019-01-01"
-						max="2022-12-31"
-						onChange={dateChnageHandler}
-						value={date}
-					/>
-				</div>
-			</div>
-			<div className="new-expense__actions ">
-				<button type="submit">Add Expense</button>
-			</div>
-		</form>
+			)}
+		</>
 	);
 };
 
